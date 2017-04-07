dfab60b05e23a1e53a5853a931e3ff10466845d0
Jacob Garcia <jgarcia224@mail.csuchico.edu>
2017-04-06 23:48:14 -0700

    so far so good but i cant get the text to work

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,10 +1,21 @@
 function writeTweet()
-{
-  
+{var randindex = intro = ["it has always been", "it would apear that","in our darkest hour", "A wise man once said"]
+body = ["for the good of humanity","John Cena","sugar, spice, everything nice", "problem after problem", "Arceus destroyer of worlds"],
+end = ["is the answer"," eror 404 entity not found"];
+  var sentence = "intro [randindex]" + "body [randindex]" + "end [randindex]"
 }
 
 //Generates a random whole number between 0 and max (inclusive)
-function randomUpTo(max)
-{
+function randomUpTo()
+{getElementById('randindex').innerHTML = intro ["*"];
   return Math.floor(Math.random() * (max+1));
+}
+
+function randomUpTo(){
+  getElementById('randindex').innerHTML = body ["*"];
+    return Math.floor(Math.random() * (max+1));
+}
+function randomUpTo(){
+getElementById('randindex').innerHTML = end ["*"];
+    return Math.floor(Math.random() * (max+1));
 }
